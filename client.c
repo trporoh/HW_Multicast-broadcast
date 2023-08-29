1b429372cf33d3582b405f30195126b786764514
vboxuser <vboxuser@linux2.myguest.virtualbox.org>
2023-08-29 19:32:56 +0300

    add client(separated becouse of changing on different vertual mashines)

--- client.c
+++ client.c
@@ -6,15 +6,16 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
-#define ADDR "192.168.1.5"
-#define PORT 2244
+#define ADDR "192.168.0.3"
+#define PORT 8888
 
 int main(int argc, char** argv) {
+    int size = 1;
 
     char* buffer = malloc(64);
     socklen_t addr_size;
 
-    struct sockaddr_in client;
+    struct sockaddr_in client, receiver;
 
     int fd = socket(AF_INET, SOCK_DGRAM, 0);
     if (-1 == fd) {
@@ -23,9 +24,11 @@ int main(int argc, char** argv) {
     }
 
     memset(&client, '\0', sizeof(struct sockaddr_in));
+    memset(&receiver, '\0', sizeof(struct sockaddr_in));
     client.sin_family = AF_INET;
     client.sin_port = htons(PORT);
-    client.sin_addr.s_addr = inet_addr(ADDR);
+    client.sin_addr.s_addr = INADDR_ANY;
+    //setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &size, sizeof(size));
 
     if (-1 == bind(fd, (struct sockaddr*)&client, sizeof(struct sockaddr_in))) {
         perror("bind ERR");
@@ -34,7 +37,7 @@ int main(int argc, char** argv) {
 
     bzero(buffer, 64);
     addr_size = sizeof(client);
-    recvfrom(fd, buffer, 64, 0, (struct sockaddr*)&client, &addr_size);
+    recvfrom(fd, buffer, 64, 0, (struct sockaddr*)&receiver, &addr_size);
     printf("[+]Data recv: %s\n", buffer);
 
     close(fd);

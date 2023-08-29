21a54117a7aa6df897a915b35cf84ad4318fd20e
vboxuser <vboxuser@linux.myguest.virtualbox.org>
2023-08-29 19:32:23 +0300

    add broadcast

--- broadcast.c
+++ broadcast.c
@@ -8,7 +8,7 @@
 #include <linux/ip.h>
 
 #define broadcast "192.168.0.255"
-#define ADDR "192.168.0.3"
+#define ADDR "192.168.0.2"
 #define SPORT 7777
 #define DPORT 8888
 
@@ -16,30 +16,9 @@
 int
 main(int argc, char* argv[])
 {
-    //char* msg = (char*)malloc(sizeof(struct udphdr) + 28);
-    //char* ip = (char*)malloc(sizeof(struct iphdr) + sizeof(struct udphdr) + 28);
-
-    /*((struct iphdr*)ip)->version = 4;
-    ((struct iphdr*)ip)->ihl = 5;
-    ((struct iphdr*)ip)->tos = 0;
-    ((struct iphdr*)ip)->tot_len = 40;
-    ((struct iphdr*)ip)->id = htons(15);
-    ((struct iphdr*)ip)->frag_off = htons(0);
-    ((struct iphdr*)ip)->ttl = 4;
-    ((struct iphdr*)ip)->protocol = IPPROTO_UDP;
-    ((struct iphdr*)ip)->check = 0;
-    ((struct iphdr*)ip)->saddr = inet_addr(ADDR);
-    ((struct iphdr*)ip)->daddr = inet_addr(broadcast);*/
-
-
-   /* ((struct udphdr*)msg)->source = htons(SPORT);
-    ((struct udphdr*)msg)->dest = htons(DPORT);
-    ((struct udphdr*)msg)->len = htons(sizeof(struct udphdr) + 20);
-    ((struct udphdr*)msg)->check = htons(0);*/
-
-   //strcpy(ip + sizeof(struct iphdr), msg);
-   //strcpy(ip + sizeof(struct iphdr) + sizeof(struct udphdr), "HELLO!?");
+   
     char* ip = malloc(64);
+    int size = 1;
     strcpy(ip, "Hello everyone!\n");
     printf("%s\n", ip);
 
@@ -58,12 +37,18 @@ main(int argc, char* argv[])
     serv.sin_addr.s_addr = inet_addr(broadcast);
     socklen_t addrlen = sizeof(serv);
 
-    if (-1 == sendto(fd, ip, strlen(ip), 0, (struct sockaddr*)&serv, addrlen))
-    {
-        perror("send ERR");
-        exit(EXIT_FAILURE);
+    if(-1 == setsockopt(fd, SOL_SOCKET, SO_BROADCAST, &size, sizeof(size))){
+	perror("socket option ERR");
+	exit(EXIT_FAILURE);
+    }	
+    for(int i = 0; i < 100; i++){
+    	if (-1 == sendto(fd, ip, strlen(ip), 0, (struct sockaddr*)&serv, addrlen))
+    	{
+    	    perror("send ERR");
+    	    exit(EXIT_FAILURE);
+    	}
     }
-    //close(fd);
+    close(fd);
 
     exit(EXIT_SUCCESS);
 }
